9c31394989bfdbfdf0eb212daf36e9756e99d032
piotrchojnacki <piotr.t.chojnacki@gmail.com>
2015-01-02 08:43:17 +0100

    Iteration H1: Sending Confirmation Emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,16 @@ Depot::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:                "ror.depot.gmail.com",
+    port:                   "587",
+    domain:                 "domain.of.sender.net",
+    authentication:         "plain",
+    user_name:              "dave", 
+    password:               "mnbvcxz1234567890", 
+    enable_starttls_auto:   true
+  }
 end
