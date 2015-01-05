b8074d01fd3c21bb314de9b5e81c88b5a8edbe69
piotrchojnacki <piotr.t.chojnacki@gmail.com>
2015-01-05 09:25:59 +0100

    Iteration I3: Limiting Access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :authorize
   protect_from_forgery with: :exception
 
   before_action :set_date_loaded
@@ -9,4 +10,11 @@ class ApplicationController < ActionController::Base
   def set_date_loaded
     @date_loaded = Time.now
   end
+
+  protected
+  	def authorize
+  		unless User.find_by(id:session[:user_id])
+  			redirect_to login_url, notice: "Place log in"
+  		end
+  	end
 end
